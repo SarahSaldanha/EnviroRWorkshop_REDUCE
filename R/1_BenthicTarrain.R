c831d65bd93d04883419350474e4b5f6d9d82fbf
Sarah Saldanha <sarahsaldanha9@gmail.com>
2025-04-04 17:28:14 +0200

    Update 1_BenthicTarrain.R

--- R/1_BenthicTarrain.R
+++ R/1_BenthicTarrain.R
@@ -4,7 +4,9 @@
 # Course: Environmental Data Extraction and Analysis from Satellite Telemetry and Other Sources
 #
 # Author: Sarah Saldanha
-# Email: 
-# Last revision: 
+# Email: sarahsaldanha9@gmail.com
+# Last revision: 2025-04-04
 #
 #-------------------------------------------------------------------------------
+##first read in the dataset we will be working with
+dataset<-read.csv2("input/dataset.csv")
